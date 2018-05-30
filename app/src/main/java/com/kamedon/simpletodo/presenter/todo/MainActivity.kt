c955b4d8b407d014e271a87ddef5752d6f16bd02
kamedon <hkamei@gmail.com>
2018-05-30 17:00:37 +0900

    toolbarを導入

--- app/src/main/java/com/kamedon/simpletodo/presenter/todo/MainActivity.kt
+++ app/src/main/java/com/kamedon/simpletodo/presenter/todo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.kamedon.simpletodo.presenter.todo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.kamedon.simpletodo.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
     }
 }
