ba763fd1c150e3a47685ef54d5006a42b0b22566
kamedon <hkamei@gmail.com>
2018-05-30 18:56:56 +0900

    repository簡易の実装

--- app/src/main/java/com/kamedon/simpletodo/domain/repository/IssueRepository.kt
+++ app/src/main/java/com/kamedon/simpletodo/domain/repository/IssueRepository.kt
@@ -4,10 +4,15 @@ import com.kamedon.simpletodo.domain.entity.Issue
 
 interface IIssueRepository {
     suspend fun save(issue: Issue)
-
+    suspend fun allIssue(): List<Issue>
 }
 
 class IssueRepository : IIssueRepository {
+    val list = mutableListOf<Issue>()
+
     override suspend fun save(issue: Issue) {
+        list.add(issue)
     }
+
+    override suspend fun allIssue() = list
 }
